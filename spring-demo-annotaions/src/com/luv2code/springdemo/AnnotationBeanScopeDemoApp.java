d6e5ba2473c0c4293045c51df5fd0ca0874f74f5
dooye0ng <hscaudy20@gmail.com>
2020-12-19 18:37:18 +0900

    update

--- spring-demo-annotaions/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
+++ spring-demo-annotaions/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -2,13 +2,18 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AnnotaionDemoApp {
+public class AnnotationBeanScopeDemoApp {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context =
+
+		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
+
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
-		System.out.println(theCoach.getDailyWorkout());
-		System.out.println(theCoach.getDailyFortune());
-		context.close();
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println(theCoach);
+		System.out.println(alphaCoach);
+		
 	}
+
 }
--- spring-demo-annotaions/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotaions/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,36 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(@Qualifier("randomFortuneService") FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Practice backhand volley !";
 	}
+	
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
 
 }
+
+
+
+
+
+
+
